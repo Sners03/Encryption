7a15e9e5555614e38a2e3aa5aeddf5eea18c30f4
Sners03 <reichelarslukas@gmail.com>
2020-03-25 21:53:41 +0100

    Minor Changes added Fuction 'adjust' added 'main' function added german comments

--- ceaser.py
+++ ceaser.py
@@ -1,12 +1,20 @@
-input_text = input('Geben sie einen Text ein: ').lower()
-
-code = int(input('Geben sie nun ein um wie viele Zeichen der Text verschoben werden soll: '))
-
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+def adjust_Data(text, code):
+    '''Hilfsfunktion zum anpassen der Daten'''
+    text = text.lower()
 
+    while code > 26 or code < 0:
+        '''
+        Schleife um sicherzustellen,  dass der Angegebene Code-Schlüssel sich 
+        im Bereich zwischen 0 und 26 befindet.
+        '''
+        if code > 26:
+            code -= 26
+        if code < 0:
+            code += 26
 
-def caeser_Encode(input_text,code):
+def caeser_Encode(input_text, code):
 
     replace_alphabet = alphabet[code:] + alphabet[:code]
     encoded_text=''
@@ -20,7 +28,11 @@ def caeser_Encode(input_text,code):
             encoded_text += i
     return encoded_text
 
-def caeser_Decode(encoded_text,code):
+def caeser_Decode(encoded_text, code):
     pass
 
-print(caeser_Encode(input_text,code))
+if __name__=='__main__':
+    input_text = input('Geben sie einen Text ein: ').lower()
+    code = int(input('''Geben sie nun ein um wie viele Zeichen der Text verschoben werden soll: '''))
+
+    print(caeser_Encode(input_text,code))
