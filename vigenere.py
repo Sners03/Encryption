188eff0545cb2e3bcce573555d5e1ea9f61af01e
Sners03 <reichelarslukas@gmail.com>
2020-03-27 12:50:08 +0100

    vignere_Encode is working

--- vigenere.py
+++ vigenere.py
@@ -12,11 +12,15 @@ def vignere_Encode(input_text, key):
     encoded_text = ''
 
     for i in input_text:
+        try:
 
-        key_char = key[key_pos]
-        encoded_char_pos = (alphabet.index(i) + alphabet.index(key_char)) % 26
+            key_char = key[key_pos]
+            encoded_char_pos = (alphabet.index(i) + alphabet.index(key_char)) % 26
 
-        encoded_text += alphabet[encoded_char_pos]
+            encoded_text += alphabet[encoded_char_pos]
+
+        except Exception:
+            encoded_text += i
 
         if key_pos != key_length-1:
             key_pos += 1
@@ -24,3 +28,12 @@ def vignere_Encode(input_text, key):
             key_pos = 0
 
     return encoded_text
+
+if __name__ == '__main__':
+
+    input_text = input('Geben sie einen Text ein: ').lower()
+    key = input('Geben sie ihr schlüsselwort ein: ').lower()
+
+    encoded_text = vignere_Encode(input_text, key)
+
+    print(encoded_text)
