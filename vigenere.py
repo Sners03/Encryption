86dda8da6118436aba0185911815ac1998f821c4
Sners03 <reichelarslukas@gmail.com>
2020-03-27 12:40:20 +0100

    Added Vigenere Encode

--- vigenere.py
+++ vigenere.py
@@ -2,3 +2,25 @@
 # Vigenére-Verschlüsselung 
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# c = Zeichen ((zahl(K)+zahl(e)) Modulo 26)
+
+def vignere_Encode(input_text, key):
+
+    key_length = len(key)
+    key_pos = 0
+    encoded_text = ''
+
+    for i in input_text:
+
+        key_char = key[key_pos]
+        encoded_char_pos = (alphabet.index(i) + alphabet.index(key_char)) % 26
+
+        encoded_text += alphabet[encoded_char_pos]
+
+        if key_pos != key_length-1:
+            key_pos += 1
+        else:
+            key_pos = 0
+
+    return encoded_text
