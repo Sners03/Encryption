274945ed1d843bbbcf564c7526af0077b3683e40
Sners03 <reichelarslukas@gmail.com>
2020-03-28 17:32:59 +0100

    finished poly caesar gui

--- gui.py
+++ gui.py
@@ -98,10 +98,62 @@ def destroy_Mono_Caesar():
     mono_Caesar_Button_6.grid_remove()
 
 def create_Poly_Caesar():
-    pass
+    poly_Caesar_Label_head.grid(row=0, column=1)
+
+    # Encoding part
+    poly_Caesar_Label_subhead_1.grid(row=1, column=0)
+
+    poly_Caesar_Label_1.grid(row=2, column=0)
+    poly_Caesar_Label_2.grid(row=2, column=1)
+    poly_Caesar_Label_3.grid(row=3, column=1)
+
+    poly_Caesar_Entry_1.grid(row=3, column=0)
+
+    poly_Caesar_Button_1.grid(row=4, column=0)
+    poly_Caesar_Button_2.grid(row=4, column=1)
+
+    placeholder_Label.grid(row=5, column=0)
+
+    # Decoding part
+    poly_Caesar_Label_subhead_2.grid(row=6, column=0)
+
+    poly_Caesar_Label_4.grid(row=7, column=0)
+    poly_Caesar_Label_5.grid(row=7, column=1)
+    poly_Caesar_Label_6.grid(row=8, column=1)
+
+    poly_Caesar_Entry_2.grid(row=8, column=0)
+
+    poly_Caesar_Button_3.grid(row=9, column=0)
+    poly_Caesar_Button_4.grid(row=9, column=1)
 
 def destroy_Poly_Caesar():
-    pass
+    poly_Caesar_Label_head.grid_remove()
+
+    # Encoding part
+    poly_Caesar_Label_subhead_1.grid_remove()
+
+    poly_Caesar_Label_1.grid_remove()
+    poly_Caesar_Label_2.grid_remove()
+    poly_Caesar_Label_3.grid_remove()
+
+    poly_Caesar_Entry_1.grid_remove()
+
+    poly_Caesar_Button_1.grid_remove()
+    poly_Caesar_Button_2.grid_remove()
+
+    placeholder_Label.grid_remove()
+
+    # Decoding part
+    poly_Caesar_Label_subhead_2.grid_remove()
+
+    poly_Caesar_Label_4.grid_remove()
+    poly_Caesar_Label_5.grid_remove()
+    poly_Caesar_Label_6.grid_remove()
+
+    poly_Caesar_Entry_2.grid_remove()
+
+    poly_Caesar_Button_3.grid_remove()
+    poly_Caesar_Button_4.grid_remove()
 
 def create_Vigenere():
     pass
@@ -146,7 +198,8 @@ def load_vigenere():
 
     root.state = 2
 
-def use_mono_Caesar_Encode():
+#Mono Caesar Code use
+def use_Mono_Caesar_Encode():
     text = mono_Caesar_Entry_1.get()
     key = int(mono_Caesar_Entry_2.get())
 
@@ -155,7 +208,7 @@ def use_mono_Caesar_Encode():
     mono_Caesar_Label_4.config(text=encoded_text)
     print(encoded_text)
 
-def use_mono_Caesar_Decode():
+def use_Mono_Caesar_Decode():
     text = mono_Caesar_Entry_3.get()
     key = int(mono_Caesar_Entry_4.get())
 
@@ -164,17 +217,41 @@ def use_mono_Caesar_Decode():
     mono_Caesar_Label_8.config(text=decoded_text)
     print(decoded_text)
 
-def clear_Entry_1():
+#Poly Caesar Code use 
+def use_Poly_Caesar_Encode():
+    text = poly_Caesar_Entry_1.get()
+
+    encoded_text = pc.poly_Caesar_Encode(text)
+
+    poly_Caesar_Label_3.config(text=encoded_text)
+    print(encoded_text)
+
+def use_Poly_Caesar_Decode():
+    text = poly_Caesar_Entry_2.get()
+
+    decoded_text = pc.poly_Caesar_Decode(text)
+
+    poly_Caesar_Label_6.config(text=decoded_text)
+    print(decoded_text)
+
+
+def mono_Clear_Entry_1():
     mono_Caesar_Entry_1.delete(0,'end')
 
-def clear_Entry_2():
+def mono_Clear_Entry_2():
     mono_Caesar_Entry_2.delete(0,'end')
 
-def clear_Entry_3():
+def mono_Clear_Entry_3():
     mono_Caesar_Entry_3.delete(0,'end')
 
-def clear_Entry_4():
+def mono_Clear_Entry_4():
     mono_Caesar_Entry_4.delete(0,'end')
+
+def poly_Clear_Entry_1():
+    mono_Caesar_Entry_1.delete(0,'end')
+
+def poly_Clear_Entry_2():
+    mono_Caesar_Entry_2.delete(0,'end')
 
 def OpenFile():
     root.filename =  filedialog.askopenfilename(initialdir = "/",
@@ -195,6 +272,8 @@ if __name__ == '__main__':
 
     placeholder_Label = Label(root,  text=' ')
 
+    #Mono Caesar
+    #Encode
     mono_Caesar_Label_head = Label(root, 
         text='''  Caeser-Verschlüsselung nach 
 Monoalphabetischer Substitution''',
@@ -210,31 +289,62 @@ Monoalphabetischer Substitution''',
     mono_Caesar_Entry_2 = Entry(root)
 
     mono_Caesar_Button_1 = Button(root, text="Eingabefeld leeren",
-                                        command=clear_Entry_1)
+                                        command=mono_Clear_Entry_1)
     mono_Caesar_Button_2 = Button(root, text="Eingabefeld leeren",
-                                        command=clear_Entry_2)
+                                        command=mono_Clear_Entry_2)
     mono_Caesar_Button_3 = Button(root, text="anwenden", 
-                                        command=use_mono_Caesar_Encode)
-
+                                        command=use_Mono_Caesar_Encode)
+    #Decode
     mono_Caesar_Label_subhead_2 = Label(root,   text='Entschlüsselung',
                                                 font=("Courier", 10))    
     mono_Caesar_Label_5 = Label(root, text='verschlüsselter Text')
     mono_Caesar_Label_6 = Label(root, text='Schlüsselzahl')
     mono_Caesar_Label_7 = Label(root, text='lesbarer Text')
-    mono_Caesar_Label_8 = Label(root, text='hallo Welt!')
+    mono_Caesar_Label_8 = Label(root, text='hallo welt!')
 
     mono_Caesar_Entry_3 = Entry(root)
     mono_Caesar_Entry_4 = Entry(root)
 
     mono_Caesar_Button_4 = Button(root, text="Eingabefeld leeren",
-                                        command=clear_Entry_3)
+                                        command=mono_Clear_Entry_3)
     mono_Caesar_Button_5 = Button(root, text="Eingabefeld leeren",
-                                        command=clear_Entry_4)
+                                        command=mono_Clear_Entry_4)
     mono_Caesar_Button_6 = Button(root, text="anwenden", 
-                                        command=use_mono_Caesar_Decode)
+                                        command=use_Mono_Caesar_Decode)
 
+    #Poly Caesar
+    #Encode
+    poly_Caesar_Label_head = Label(root, 
+        text='''  Caeser-Verschlüsselung nach 
+Polyalphabetischer Substitution''',
+        font=("Courier", 11))
+    poly_Caesar_Label_subhead_1 = Label(root,   text='Verschlüsselung',
+                                                font=("Courier", 10))
+    poly_Caesar_Label_1 = Label(root, text='lesbarer Text')
+    poly_Caesar_Label_2 = Label(root, text='verschlüsselter Text')
+    poly_Caesar_Label_3 = Label(root, text='hbnos cltc!')
 
-    #Mono Caeser is loaded first
+    poly_Caesar_Entry_1 = Entry(root)
+
+    poly_Caesar_Button_1 = Button(root, text="Eingabefeld leeren",
+                                        command=poly_Clear_Entry_1)
+    poly_Caesar_Button_2 = Button(root, text="anwenden", 
+                                        command=use_Poly_Caesar_Encode)
+    #Decode
+    poly_Caesar_Label_subhead_2 = Label(root,   text='Entschlüsselung',
+                                                font=("Courier", 10))    
+    poly_Caesar_Label_4 = Label(root, text='verschlüsselter Text')
+    poly_Caesar_Label_5 = Label(root, text='lesbarer Text')
+    poly_Caesar_Label_6 = Label(root, text='hallo welt!')
+
+    poly_Caesar_Entry_2 = Entry(root)
+
+    poly_Caesar_Button_3 = Button(root, text="Eingabefeld leeren",
+                                        command=poly_Clear_Entry_2)
+    poly_Caesar_Button_4 = Button(root, text="anwenden", 
+                                        command=use_Poly_Caesar_Decode)
+
+    #Mono Caesar is loaded first
     create_Mono_Caesar()
 
     create_gui()
