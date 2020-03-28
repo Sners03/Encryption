9ba2cf259254dca250416086b6e59f58015f71ea
Sners03 <reichelarslukas@gmail.com>
2020-03-28 10:56:08 +0100

    gui progress

--- gui.py
+++ gui.py
@@ -5,33 +5,10 @@ from custom_modules import vigenere as vig
 from tkinter import filedialog
 from tkinter import *
 
-
-def load_Mono_Caeser():
-    print("mono_caesar")
-
-def load_Poly_Caeser():
-    print("poly_caesar")
-
-def load_vigenere():
-    print("poly_caesar")
-
-def OpenFile():
-    root.filename =  filedialog.askopenfilename(initialdir = "/",
-                                            title = "Select file",
-                                            filetypes = (("txt files","*.txt")
-                                                        ,("all files","*.*")))
-
-    print(root.filename)
-
-def About():
-    print("This is a simple example of a menu")
-
-
-if __name__ == '__main__':
+def create_gui():
     encoded = mc.mono_Caesar_Encode('Hallo Welt', 2)
     print(encoded)
 
-    root = Tk()
     menu = Menu(root)
     root.config(menu=menu)
     filemenu = Menu(menu)
@@ -46,4 +23,52 @@ if __name__ == '__main__':
     menu.add_cascade(label="Help", menu=helpmenu)
     helpmenu.add_command(label="About...", command=About)
 
-    mainloop()
+    root.mainloop()
+
+def get_state():
+    return root.state
+
+def create_Mono_Caesar():
+    mono_Caesar_Entry_1.grid(row=1, column=1)
+
+def destroy_Mono_Caesar():
+    mono_Caesar_Entry_1.grid_remove()
+
+
+def load_Mono_Caeser():
+    create_Mono_Caesar()
+    root.state = 0
+
+    print(root.state)
+    print("mono_caesar")
+
+def load_Poly_Caeser():
+    destroy_Mono_Caesar()
+    root.state = 1
+
+    print(root.state)
+    print("poly_caesar")
+
+def load_vigenere():
+    print("vigenere")
+
+def OpenFile():
+    root.filename =  filedialog.askopenfilename(initialdir = "/",
+                                            title = "Select file",
+                                            filetypes = (("txt files","*.txt")
+                                                        ,("all files","*.*")))
+
+    print(root.filename)
+
+def About():
+    print("This is a simple example of a menu")
+
+
+if __name__ == '__main__':
+    root = Tk()
+
+    root.state = 0
+
+    mono_Caesar_Entry_1 = Entry(root)
+
+    create_gui()
